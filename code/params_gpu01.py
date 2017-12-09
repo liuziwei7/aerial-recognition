2437f286d63550dca77a9184d9741f6d7152b422
Ziwei Liu <zwliu.hust@gmail.com>
2017-12-08 17:30:56 -0800

    small tweaks

--- code/params_gpu01.py
+++ code/params_gpu01.py
@@ -61,11 +61,12 @@ use_deform = False
 cnn_adam_learning_rate = 1e-5
 cnn_adam_loss = 'categorical_crossentropy'
 # cnn_epochs = 50
-cnn_epochs = 2
+cnn_epochs = 5
 	
 lstm_adam_learning_rate = 1e-4
 lstm_loss = 'categorical_crossentropy'
-lstm_epochs = 100
+# lstm_epochs = 100
+lstm_epochs = 20
 
 #DIRECTORIES AND FILES
 directories = {}
